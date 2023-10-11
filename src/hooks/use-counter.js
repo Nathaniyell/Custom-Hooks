d9580f8cc243495530de5f953d0e4ec1dbc3cc6b
Nathaniyell <essien.nathan@yahoo.com>
2023-10-11 05:33:49 +0100

    restructure the custom hook to accepts props

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCount) => prevCount + 1);
+      if (forwards) {
+        setCounter((prevCount) => prevCount + 1);
+      } else {
+        setCounter((prevCount) => prevCount - 1);
+      }
     }, 1000);
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
-  return counter 
+  return counter;
 };
-//the return 
+//the return keyword is used to return whichever variable or constants you want to be accessed by components that will make use of your customHook
+
+/*
+-A mechanism of reusing logic just as regular functions are
+custom hooks can use other react hooks and other custom -hooks they can leverage useState, useEffect etc.
+*/
 export default useCounter;
