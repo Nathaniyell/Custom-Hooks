22115c8dbc28ae3a15eb807345f0510446455976
Nathaniyell <essien.nathan@yahoo.com>
2023-10-13 23:23:50 +0100

    restructure newTask component

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -6,30 +6,26 @@ import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
 
-  const{ error, isLoading, sendRequest } = useHttp({url: 'https://fetchmovies-udemy-default-rtdb.firebaseio.com/tasks.json', method: "POST",  body: text.taskText ,
-         headers: {
-           'Content-Type': 'application/json',
-         },})
+  const{ error, isLoading, sendRequest:sendTaskRequest } = useHttp()
+  const createTask=(taskData)=>{
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskData.taskText };
 
+    props.onAddTask(createdTask);
+  }
  
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+  const enterTaskHandler = async(taskText)=>{
+    sendTaskRequest({url: 'https://fetchmovies-udemy-default-rtdb.firebaseio.com/tasks.json', 
+    method: "POST",
+    body: {text:taskText} ,
+    headers: {
+      'Content-Type': 'application/json',
     }
-    setIsLoading(false);
-  };
+  }, createTask)
+  }
 
-  return (
+     return (
     <Section>
       <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
       {error && <p>{error}</p>}
