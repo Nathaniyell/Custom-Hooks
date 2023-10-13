46f26bfe549b98806df4c97acdc3aee44710e7a1
Nathaniyell <essien.nathan@yahoo.com>
2023-10-13 21:05:32 +0100

    return object from custom hook to be accessed by other components

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,43 +1,36 @@
 import { useState } from "react";
 
+const useHttp = (requestConfig) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-const useHttp=(requestConfig)=>{
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-   
-  
-    const sendRequest  = async (taskText) => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(
-       requestConfig.url,{
-       method: requestConfig.method,
+  const sendRequest = async (taskText, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
         headers: requestConfig.headers,
-        body: JSON.stringify(requestConfig.body)
-       }
-        );
-  
-        if (!response.ok) {
-          throw new Error('Request failed!');
-        }
-  
-        const data = await response.json();
-  
-        const loadedTasks = [];
-  
-        for (const taskKey in data) {
-          loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-        }
-  
-        setTasks(loadedTasks);
-      } catch (err) {
-        setError(err.message || 'Something went wrong!');
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
       }
-      setIsLoading(false);
-    };
 
-}
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
 
+  return {
+    sendRequest,
+    error,
+     isLoading,
+  };
+};
 
-export default useHttp
+export default useHttp;
