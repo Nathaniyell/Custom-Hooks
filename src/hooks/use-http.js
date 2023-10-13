f2b2df6d908e52ede4655e0f3f2ec90208c4dede
Nathaniyell <essien.nathan@yahoo.com>
2023-10-13 14:15:38 +0100

    add parameters to be accepted when hook is used

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 
 
-const useHttp=()=>{
+const useHttp=(requestConfig)=>{
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
    
   
-    const fetchTasks = async (taskText) => {
+    const sendRequest  = async (taskText) => {
       setIsLoading(true);
       setError(null);
       try {
         const response = await fetch(
-          'https://fetchmovies-udemy-default-rtdb.firebaseio.com/tasks.json'
+       requestConfig.url,{
+       method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body)
+       }
         );
   
         if (!response.ok) {
